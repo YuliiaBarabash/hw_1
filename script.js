aaa765ecd97b545433d2b46f2dbc107c4d517b50
Yuliia Barabash <yuliiabarabash@MacBook-Pro-Yuliia.local>
2023-05-02 12:43:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstNumber = Number (prompt ('Please enter your first number'));
+const secondNumber = Number (prompt ('Please enter your second number'));
+const thirdNumber = Number (prompt ('Please enter your third number'));
+const result = (firstNumber + secondNumber + thirdNumber) / 3;
+alert(parseFloat(result));
